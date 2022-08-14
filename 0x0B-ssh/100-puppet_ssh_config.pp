bf8a2f09b9258a46c31cc6266e6d394c9c52ed67
Karoline-S <karoline.amy@gmail.com>
2022-08-13 21:08:14 -0700

    fix - change path to provider

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,8 @@
 # apply changes to ssh-config file
 
 exec { 'update_ssh_config':
-  command => 'echo "Host 54.242.211.172\n\tIdentifyFile ~/.ssh/school\n\tPasswordAuthentication no" >> /etc/ssh/ssh_config',
-  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  => 'echo "Host 54.242.211.172
+    IdentifyFile ~/.ssh/school
+    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  provider => 'shell',
 }
