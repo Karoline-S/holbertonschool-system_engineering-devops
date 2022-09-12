d8543cee80897778e1bdc66ce1ecfc342d730fbd
Karoline-S <karoline.amy@gmail.com>
2022-09-12 05:02:02 -0700

    fix - add name == main

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,29 +4,30 @@
 import requests
 from sys import argv
 
-try:
-    if int(argv[1]):
-        url_users = 'https://jsonplaceholder.typicode.com/users/{}' \
-            .format(argv[1])
-        url_todos = 'https://jsonplaceholder.typicode.com/users/{}/todos' \
-            .format(argv[1])
-except Exception:
-    print('Usage: python3 0-gather_data_from_an_API.py <integer>')
-    exit
 
-user_data = requests.get(url_users).json()
-todo_data = requests.get(url_todos).json()
+def get_data(id):
+    """
+    fetches data from REST API and displays formatted response
+    """
+    url_users = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
+    url_todos = 'https://jsonplaceholder.typicode.com/users/{}/todos' \
+        .format(argv[1])
 
-total_tasks = len(todo_data)
-completed_tasks = 0
+    user_data = requests.get(url_users).json()
+    todo_data = requests.get(url_todos).json()
+    total_tasks = len(todo_data)
+    completed_tasks = 0
 
-for item in todo_data:
-    if item['completed']:
-        completed_tasks += 1
+    for item in todo_data:
+        if item['completed']:
+            completed_tasks += 1
 
-print('Employee {} is done with tasks({}/{}):'
-      .format(user_data['name'], completed_tasks, total_tasks))
+    print('Employee {} is done with tasks({}/{}):'
+          .format(user_data['name'], completed_tasks, total_tasks))
 
-for item in todo_data:
-    if item['completed']:
-        print('\t {}'.format(item['title']))
+    for item in todo_data:
+        if item['completed']:
+            print('\t {}'.format(item['title']))
+
+if __name__ == '__main__':
+    get_data(int(argv[1]))
