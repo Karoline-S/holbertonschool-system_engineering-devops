16cb6d3492e52c5028c6c91186367a83d4054169
Karoline-S <karoline.amy@gmail.com>
2022-09-11 20:08:40 -0700

    build - add task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""call a REST API and display formatted response"""
+
+import requests
+import csv
+from sys import argv
+
+try:
+    if int(argv[1]):
+        url_users = 'https://jsonplaceholder.typicode.com/users/{}' \
+            .format(argv[1])
+        url_todos = 'https://jsonplaceholder.typicode.com/users/{}/todos' \
+            .format(argv[1])
+except Exception:
+    print('Usage: python3 0-gather_data_from_an_API.py <integer>')
+    exit
+
+user_data = requests.get(url_users).json()
+todo_data = requests.get(url_todos).json()
+
+user_id = str(user_data['id'])
+user_name = user_data['username']
+filename = '{}.csv'.format(argv[1])
+
+with open(filename, 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+
+    for item in todo_data:
+        task_status = str(item['completed'])
+        task_title = item['title']
+        row = [user_id, user_name, task_status, task_title]
+        writer.writerow(row)
