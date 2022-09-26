27c79b8710bd0db825f8461652f17ec04b400d4c
Karoline-S <karoline.amy@gmail.com>
2022-09-26 16:02:29 -0700

    build - add task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" sends get request to reddit api """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ call reddit api for subreddit and retrieve
+    number of subscribers from the response """
+
+    if type(subreddit) != str:
+        raise TypeError("subreddit must be a string")
+
+    url = 'http://reddit.com/r/' + subreddit + '/about.json'
+    headers = {'User-Agent':
+               'python3/Holberton_school_project/1.0 (by /u/karoline_dev)'}
+
+    response = requests.get(url, headers=headers)
+    if response:
+        body = response.json()
+        try:
+            return body['data']['subscribers']
+        except KeyError:
+            return 0
